9892b78f9e3967836950c9f99bc4d401a9724d87
japc <japc.grafico@gmail.com>
2020-09-30 16:29:48 +0200

    Add Component AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
-
-	// const categories = ['One Pusch', 'Samurai X', 'Dragon Ball'];
 
 	// Uso del Hook useState para modificar dinamicamente la lista
 	const [categories, setCategories] = useState(['One Pusch', 'Samurai X', 'Dragon Ball']);
 
-
+	/*
 	// Tarea. Agregar un elemento nuevo al arrary mediante esta funcion.
 	const handleAdd = () => {
 		// setCategories( [...categories, 'Regular Show'] );
@@ -17,12 +16,13 @@ export const GifExpertApp = () => {
 		//setCategories puede tener un callback en el cual el primer argumetno es el valor del estado anterior, en este caso c es el array de categories, y luego tiene que retornar el nuevo estado. En este acaso el array con spread + el nuevo elmento.
 		setCategories(c => [...c, 'Regular Show']);
 	};
+ 	*/
 
 	return (
 		<>
 			<h2>GifExpertApp</h2>
+			<AddCategory setCategories = { setCategories }/>
 			<hr/>
-			<button onClick={ handleAdd }>Agregar</button>
 
 			<ol>
 				{
