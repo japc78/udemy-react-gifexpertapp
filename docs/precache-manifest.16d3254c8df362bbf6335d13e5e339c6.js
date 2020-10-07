e86911cf75b7c5eb43748c9e0562393de564dae2
japc <japc.grafico@gmail.com>
2020-10-07 08:20:08 +0200

    Rename buils to docs for github pages

---
+++ docs/precache-manifest.16d3254c8df362bbf6335d13e5e339c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf7c1d411a80af367ccb06c58779086",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4aa4cc3f909c44cedb0",
+    "url": "/static/css/main.818516da.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4aa4cc3f909c44cedb0",
+    "url": "/static/js/main.2c687780.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
